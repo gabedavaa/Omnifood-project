04a1061ec4735e06a28b56253462adefffe8d9e1
gabedavaa <110985736+gabedavaa@users.noreply.github.com>
2023-04-09 23:20:07 +0400

    implementing smooth scrolling

--- js/script.js
+++ js/script.js
@@ -1,1 +1,42 @@
-console.log("ff");
+"use strict";
+
+////////////////////////////
+// Set current year
+const yearElement = document.querySelector(".year");
+yearElement.textContent = new Date().getFullYear();
+
+// Make mobile navigation work
+const headerElement = document.querySelector(".header");
+const btnNav = document.querySelector(".btn-mobile-nav");
+
+btnNav.addEventListener("click", () => {
+  headerElement.classList.toggle("nav-open");
+});
+
+///////////////////////////////////////////////
+// Smooth scrolling animation
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionElement = document.querySelector(href);
+      sectionElement.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile navigation
+    if (link.classList.contains("main-nav-link"))
+      headerElement.classList.toggle("nav-open");
+  });
+});
