27311cf6dba9efd09015a945187e6c63ccea27ea
gabedavaa <110985736+gabedavaa@users.noreply.github.com>
2023-04-10 22:24:20 +0400

    add sticky navigation

--- js/script.js
+++ js/script.js
@@ -40,3 +40,27 @@ allLinks.forEach((link) => {
       headerElement.classList.toggle("nav-open");
   });
 });
+
+//////////////////////////////////////////////////////
+// Sticky navigation
+const sectionHeroElement = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroElement);
