79c000f0770b4f47b2b0ba6312f81b165a58b72c
gabedavaa <110985736+gabedavaa@users.noreply.github.com>
2023-04-12 07:06:44 +0400

    fixing flexbox gap in safari

--- js/script.js
+++ js/script.js
@@ -48,7 +48,6 @@ const sectionHeroElement = document.querySelector(".section-hero");
 const obs = new IntersectionObserver(
   function (entries) {
     const ent = entries[0];
-    console.log(ent);
     if (!ent.isIntersecting) {
       document.body.classList.add("sticky");
     }
@@ -64,3 +63,25 @@ const obs = new IntersectionObserver(
   }
 );
 obs.observe(sectionHeroElement);
+
+///////////////////////////////////////////////////////////////////////
+//Fixing flexbox gap property missing in some Safari versions
+
+function checkFlexGap() {
+  let flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  let isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+
+checkFlexGap();
